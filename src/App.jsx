e3ff50d729c7541c5b9fc649494aa96eba7cd719
Roman Rizo <romanrizodev@gmail.com>
2024-02-15 21:08:39 -0600

    feat next prev shuffle words

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,35 @@ import {
 import { Badge } from "./components/ui/badge";
 import { useWords } from "./hooks/useWords";
 import { NewWordDialog } from "./components/Form";
+import { useState } from "react";
 
 function App() {
   const words = useWords();
   const word = words[0] ? words[~~(Math.random() * words.length)] : {};
+  const [actualWord, setActualWord] = useState(word);
+
+  const shuffleWord = () => {
+    while (true) {
+      const newWord = words[~~(Math.random() * words.length)];
+      if (newWord !== actualWord) {
+        setActualWord(newWord);
+        break;
+      }
+    }
+  };
+
+  const nextWord = () => {
+    const currentIndex = words.findIndex((el) => el.word === actualWord.word);
+    console.log(words[(currentIndex + 1) % words.length]);
+    const nextWord = words[(currentIndex + 1) % words.length];
+    setActualWord(nextWord);
+  };
+
+  const prevWord = () => {
+    const currentIndex = words.findIndex((el) => el.word === actualWord.word);
+    const prevWord = words[(currentIndex - 1 + words.length) % words.length];
+    setActualWord(prevWord);
+  };
 
   return (
     <>
@@ -51,15 +76,20 @@ function App() {
         </Button>
         <div className="flex gap-x-3.5 items-center">
           <Button
+            onClick={prevWord}
             variant="outline"
             className="p-3 w-[56px] h-[56px] aspect-square border rounded-full"
           >
             <ArrowLeftIcon className="w-full h-full" />
           </Button>
-          <Button className="p-4 w-[70px] h-[70px] aspect-square border rounded-full">
+          <Button
+            onClick={shuffleWord}
+            className="p-4 w-[70px] h-[70px] aspect-square border rounded-full"
+          >
             <ShuffleIcon className="w-full h-full" />
           </Button>
           <Button
+            onClick={nextWord}
             variant="outline"
             className="p-3 w-[56px] h-[56px] aspect-square border rounded-full"
           >
